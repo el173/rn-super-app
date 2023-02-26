3e245da3883f533820c0121073ad5da2fcbbea72
Hashith Karunarathne <hashithkarunarathne@live.com>
2023-02-26 16:50:59 +0800

    implement basic intergration with mini apps

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -5,113 +6,78 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import React, {useState} from 'react';
+import {SafeAreaView, StatusBar, StyleSheet, Text, View, Button} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Federated} from '@callstack/repack/client';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+const App1 = React.lazy(() => Federated.importModule('rnminiappone', './App'));
+const App2 = React.lazy(() => Federated.importModule('rnminiapptwo', './App'));
 
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+function App(): JSX.Element {
+
+  const [visibleApp, setVisibleApp] = useState<string>('');
+
+  const renderRelevantApp = () => {
+    switch (visibleApp) {
+      case 'app1':
+        return (
+          <View style={styles.miniAppWrapper}>
+            <React.Suspense fallback={<Text>Loading app1...</Text>}>
+              <App1 />
+            </React.Suspense>
+          </View>
+        );
+      case 'app2':
+        return (
+          <View style={styles.miniAppWrapper}>
+            <React.Suspense fallback={<Text>Loading app2...</Text>}>
+              <App2 />
+            </React.Suspense>
+          </View>
+        );
+      default:
+        return (
+          <SafeAreaView>
+            <StatusBar />
+            <View style={styles.mainContainer}>
+              <Button title="App One" onPress={() => setVisibleApp('app1')} />
+              <Button title="App Two" onPress={() => setVisibleApp('app2')} />
+            </View>
+          </SafeAreaView>
+        );
+    }
+  };
+
+  const renderBackToHome = () => {
+    if (visibleApp) {
+      return (
+        <View style={styles.backWrapper}>
+          <Button title="<- Back To Home" onPress={() => setVisibleApp('')} />
+        </View>
+      );
+    }
+  };
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View>
+      {renderBackToHome()}
+      {renderRelevantApp()}
     </View>
   );
 }
 
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  mainContainer: {
+    height: '100%',
+    justifyContent: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  miniAppWrapper: {
+    // flex:1,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  backWrapper: {
+    backgroundColor: '#E1F8DC',
+    marginTop: 50,
   },
 });
 
--- index.js
+++ index.js
@@ -2,8 +2,37 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
+import {AppRegistry, Platform} from 'react-native';
+import {ScriptManager, Script, Federated} from '@callstack/repack/client';
 import {name as appName} from './app.json';
+import App from './App';
+
+const resolveURL = Federated.createURLResolver({
+  containers: {
+    rnminiappone: 'http://localhost:8083/[name][ext]',
+    rnminiapptwo: 'http://localhost:8084/[name][ext]',
+  },
+});
+
+ScriptManager.shared.addResolver(async (scriptId, caller) => {
+  let url;
+  if (caller === 'main') {
+    url = Script.getDevServerURL(scriptId);
+  } else {
+    url = resolveURL(scriptId, caller);
+  }
+
+  if (!url) {
+    return undefined;
+  }
+
+  return {
+    url,
+    cache: false, // For development
+    query: {
+      platform: Platform.OS,
+    },
+  };
+});
 
 AppRegistry.registerComponent(appName, () => App);
